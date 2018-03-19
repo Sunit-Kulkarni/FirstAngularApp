7bf27a52a8cb9fc628b3768df395d7d29ccce6b1
Sunit Kulkarni <Sunit@Sunits-MBP-2.direcpath.net>
2018-03-19 00:03:35 -0400

    adding animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+
+        query(
+          ':enter',
+          stagger('300ms', [
+            animate(
+              '.6s ease-in',
+              keyframes([
+                style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateY(35px)',
+                  offset: 0.3
+                }),
+                style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
+              ])
+            )
+          ]),
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an Item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['howdy', 'there'];
   constructor() {}
 
   ngOnInit() {
